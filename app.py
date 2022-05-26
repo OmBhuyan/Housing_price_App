8746ac9ff1e320827d1c2f016a0930e9fb4197db
om.bhuyan <om.bhuyan@tigeranalytics.com>
2022-05-26 11:32:05 +0530

    comment

--- app.py
+++ app.py
@@ -102,11 +102,12 @@ def predict():
     )
 
 
+configure_log()
+app.logger.info("App Started.")
+test_data_url = get_artifacts()
+model = joblib.load(MODEL_PATH)
+test_data = load_housing_data(test_data_url)
+
 if __name__ == "__main__":
-    configure_log()
-    app.logger.info("App Started.")
-    test_data_url = get_artifacts()
-    model = joblib.load(MODEL_PATH)
-    test_data = load_housing_data(test_data_url)
 
     app.run(host="0.0.0.0", port=5000)
